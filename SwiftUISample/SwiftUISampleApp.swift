dc33eba01cdba3ff67796d60372ac8b0bb220c6d
Immran Fareedh <immranfareedh.syediqbal@cesltd.com>
2021-04-18 22:04:41 +0530

    Initial commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Immran on 18/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
